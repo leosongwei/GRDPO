17b6b744bcebdf3e452fe9253e7971a5e124f7e9
leo_songwei <leo_songwei@outlook.com>
2025-11-08 15:09:08 +0800

    hmmm

---
+++ question_iterator_math.py
@@ -0,0 +1,13 @@
+import json
+
+class QuestionIteratorMath:
+    def __init__(self, jsonl_file_path, max_epochs: int = 1):
+        pass
+        # load jsonl file without shuffle
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        # if max_epochs not reached, loop from begin
+        pass
