e7995660e5dd4a46a8dd96ac4c934934bc2e67b8
leo_songwei <leo_songwei@outlook.com>
2025-11-08 15:14:11 +0800

    hmmm

--- question_iterator_math.py
+++ question_iterator_math.py
@@ -1,7 +1,8 @@
 import json
 
 class QuestionIteratorMath:
-    def __init__(self, jsonl_file_path, max_epochs: int = 1):
+    def __init__(self, jsonl_file_path: str, sample_counts: int, max_epochs: int = 1):
+        # the sample file can be found at: GRDPO2/datasets/math/samples.jsonl
         pass
         # load jsonl file without shuffle
 
@@ -11,3 +12,4 @@ class QuestionIteratorMath:
     def __next__(self):
         # if max_epochs not reached, loop from begin
         pass
+        # 返回：List[Dict]，个数为sample_counts，挨个取就行
